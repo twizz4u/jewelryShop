1c3bf148527b4421cebb0c106bf5e78dc5c28edb
twizz4u <tiwatayoaragbaiye@gmail.com>
2025-02-10 23:21:30 +0100

    deployed jewelries

--- src/assets/HighlightData.jsx
+++ src/assets/HighlightData.jsx
@@ -4,28 +4,28 @@ const HighlightData = [
     subtitle: "Christmas Gift Guide",
     Date: "FEBRUARY 17, 2023",
     Description: "Oneself...",
-    image: "src/assets/images/Selective-Styles-Help-your-look.jpg",
+    image: "images/Selective-Styles-Help-your-look.jpg",
   },
   {
     title: "Alukas",
     subtitle: "Christmas Gift Guide",
     Date: "FEBRUARY 17, 2023",
     Description: "Oneself...",
-    image: "src/assets/images/How-to-Style-a-Quiff.jpg",
+    image: "images/How-to-Style-a-Quiff.jpg",
   },
   {
     title: "Alukas",
     subtitle: "Christmas Gift Guide",
     Date: "FEBRUARY 17, 2023",
     Description: "Oneself...",
-    image: "src/assets/images/Christmas-Gift-Guide.jpg",
+    image: "images/Christmas-Gift-Guide.jpg",
   },
   {
     title: "Alukas",
     subtitle: "Christmas Gift Guide",
     Date: "FEBRUARY 17, 2023",
     Description: "Oneself...",
-    image: "src/assets/images/Jewels-of-Prosperity-2023.jpg",
+    image: "images/Jewels-of-Prosperity-2023.jpg",
   },
 ];
 
